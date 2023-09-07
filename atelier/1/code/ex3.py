4e4d17c61ddb612c245197503b4ddc58934ead14
Ahmed Guetti <guettiahmed101@gmail.com>
2023-09-07 13:45:22 +0100

    Fix some bug in the code Ex3.py 🐞🐞

--- atelier/1/code/ex3.py
+++ atelier/1/code/ex3.py
@@ -1,17 +1,20 @@
 import math
 
 def discriminant(a:float ,b:float ,c:float )-> float:
-    return pow(b,2)-4*a*c
+    if a == 0:
+        return None
+    else:
+        return pow(b,2)-4*a*c
 
 def racine_unique(a:float, b:float)->float:
     if a == 0: 
-        print("[ERR] a doit etre different de 0")
+        return None
     else:
         return (-1*b)/(2*a)
 
 def racine_double(a:float ,b:float ,delta:float ,num:float) -> float:
     if a == 0: 
-        print("[ERR] a doit etre different de 0")
+        return None
     else :
         if num == 1:
             ans = (-1*b+math.sqrt(delta))/(2*a)
@@ -20,22 +23,70 @@ def racine_double(a:float ,b:float ,delta:float ,num:float) -> float:
         return ans
 
 def str_equation(a:float ,b:float ,c:float )->str:
-    #  ax2 +bx+c=0
-    return str(a)+"x²+"+str(b)+"x+"+str(c)+"= 0"
+    if b == 0:
+        if a == 1:
+            if c == 0:
+                return "x² = 0"
+            else:
+                return "x²+"+str(c)+"= 0"
+        elif a == -1:
+            if c == 0:
+                return "-x² = 0"
+            else:
+                return "-x²+"+str(c)+"= 0"
+        elif a == 0:
+            if c == 0:
+                return "[ERR] no equation"
+            else:
+                return str(c)+"= 0"
+        else:
+            if c == 0:
+                return str(a)+"x² = 0"
+            else:
+                return str(a)+"x²+"+str(c)+"= 0"
+    else:
+        if a == 1:
+            if c == 0:
+                return "x²+"+str(b)+"x = 0"
+            else:   
+                return "x²+"+str(b)+"x+"+str(c)+"= 0"
+        elif a == -1:            
+            if c == 0:
+                return "-x²+"+str(b)+"x = 0"
+            else: 
+                return "-x²+"+str(b)+"x+"+str(c)+"= 0"
+        elif a == 0:            
+            if c == 0:
+                return str(b)+"x = 0"
+            else: 
+                return str(b)+"x+"+str(c)+"= 0"
+        else:
+            if c == 0:
+                return str(a)+"x²+"+str(b)+"x = 0"
+            else: 
+                return str(a)+"x²+"+str(b)+"x+"+str(c)+"= 0"
+
+
 def solution_equation(a:float ,b:float ,c:float)->str:
     print("Solution de l'equation", end =" ")
     print(str_equation(a,b,c))
     delta = discriminant(a,b,c)
-    if delta < 0:
-        print("Pas de racine réelle")
+    if delta == None:
+        print("[ERR] a doit etre different de 0\n\n")
+    elif delta < 0:
+        print("Pas de racine réelle\n\n")
     elif delta == 0:
-        print(f"Racine unique : x= {racine_unique(a,b)}")
+        print(f"Racine unique : x= {racine_unique(a,b)} \n\n")
     else:
-        print(f"Deux racines :\n x1 = {racine_double(a,b, delta, 1)}\n x2 = {racine_double(a,b, delta, 2)}")
+        print(f"Deux racines :\n x1 = {racine_double(a,b, delta, 1)}\n x2 = {racine_double(a,b, delta, 2)}\n\n")
 
 
 def funct_test ():
-    solution_equation(1,2,1)
+    solution_equation(1,0,1)
+    solution_equation(-1,2,1)
+    solution_equation(0,2,1)
+    solution_equation(1,2,0)
+    solution_equation(12,2,0)
 
 
 funct_test()
