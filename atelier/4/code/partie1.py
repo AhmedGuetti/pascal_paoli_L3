61055a969871c53bd5e27384222ed90b0cb8baee
Ahmed Guetti <guettiahmed101@gmail.com>
2023-09-19 08:03:17 +0100

    atelier 4 ex3 ex4

---
+++ atelier/4/code/partie1.py
@@ -0,0 +1,95 @@
+import random
+import math as m
+
+def gen_list_random_int(int_nbr=10, int_binf=0, int_bsub=10)->list:
+    return [m.floor((random.random()*(int_bsub-int_binf+1))+int_binf) for e in range(int_nbr)]
+
+
+
+
+def mix_list(list_to_mix:list):
+    L = list(list_to_mix)
+    for i,e in enumerate(L):
+        j = m.floor(random.random()*(i+1))
+        L[i] , L[j] = L[j] , L[i]
+
+    return L
+
+
+def mix_list_v2(list_to_mix:list):
+    ans = []
+    fin = False
+    i = 0
+    while not fin:
+        j = m.floor(random.random()*(len(list_to_mix)))
+        if  list_to_mix[j] != None:
+            ans.append(list_to_mix[j])
+            list_to_mix[j] = None
+            i += 1
+        if i == len(list_to_mix):
+            fin = True
+
+    return ans
+
+
+def choose_element_list(list_in_which_to_choose:list):
+    return list_in_which_to_choose[random.randint(0, len(list_in_which_to_choose) - 1)]
+
+def extract_elements_list(list_in_which_to_choose:list, int_nbr_of_element_to_extract:int):
+    ans = []
+    L = list(list_in_which_to_choose)
+    for i in range(int_nbr_of_element_to_extract):
+        ran_index = random.randint(0, len(L) - 1)
+        elem = L.pop(ran_index)
+        ans.append(elem)
+    return ans
+
+def extract_elements_list_v2(list_in_which_to_choose:list, int_nbr_of_element_to_extract:int):
+    return [choose_element_list(list_in_which_to_choose) for i in range(int_nbr_of_element_to_extract)]
+
+
+
+
+
+
+
+
+
+def testing_unit():
+    l1 = ["Apple", "Asus", "Lenovo", "Dell", "MSI"]
+    # print(gen_list_random_int(100, 12,43))
+    # print(gen_list_random_int())
+    print("LA LIST MIXEE")
+    print(f"list de base = {l1} \n mix_list({l1}) = {mix_list(l1)} \n mix_list_v1({l1}) = {mix_list_v2(l1)}")
+    lst_sorted=[i for i in range(10)]
+    print(lst_sorted)
+    print("Liste triée de départ",lst_sorted)
+    mixed_list=mix_list(lst_sorted)
+    print("Liste mélangée obtenue",mixed_list)
+    print("Liste triée de départ après appel à mixList, elle doit être inchangée",lst_sorted)
+
+    print("Exercice 3")
+    # Test de votre code
+    print('Liste triée de départ',lst_sorted)
+    e1 = choose_element_list(lst_sorted)
+    print('A la première exécution',e1,'a été sélectionné')
+    e2 = choose_element_list(lst_sorted)
+    print('A la deuxième exécution',e2,'a été sélectionné')
+    assert e1 != e2
+    print("Exercice 4")
+    # Test de votre code
+    print('Liste de départ',lst_sorted)
+    sublist = extract_elements_list(lst_sorted,5)
+    print('La sous liste extraite est',sublist)
+    print('Liste de départ après appel de la fonction est',lst_sorted)
+
+
+
+
+
+def main():
+    testing_unit()
+
+
+if __name__ == "__main__":
+    main()
---
+++ atelier/4/code/partie2.py
@@ -0,0 +1,107 @@
+import time
+import random
+import math as m
+import matplotlib.pyplot as plt
+import numpy as np
+
+
+import partie1
+
+
+def perf_mix(mix_me:callable, mix_py:callable, lists_size:list, nb:int)->tuple:
+    ans = ([],[])
+    for size in  lists_size:
+        moy_me = 0
+        moy_py = 0
+        for j in range(nb):
+            L = partie1.gen_list_random_int(size,0,size*2)
+            start_pc = time.perf_counter()
+            mix_me(L)
+            end_pc = time.perf_counter()
+
+            start_pc_py = time.perf_counter()
+            mix_py(L)
+            end_pc_py = time.perf_counter()
+
+
+            moy_me += end_pc-start_pc
+            moy_py += end_pc_py-start_pc_py
+        moy_me = moy_me / nb
+        moy_py = moy_py / nb
+        ans[0].append(moy_me)
+        ans[1].append(moy_py)
+    return ans
+
+def perf_extract(extract_elements_list:callable, sample:callable, lists_size:list, nb:int)->tuple:
+    ans = ([],[])
+    for size in  lists_size:
+        moy_me = 0
+        moy_py = 0
+        for j in range(nb):
+            L = partie1.gen_list_random_int(size,0,size*2)
+            start_pc = time.perf_counter()
+            extract_elements_list(L, m.floor(random.random()*size))
+            end_pc = time.perf_counter()
+
+            start_pc_py = time.perf_counter()
+            sample(L, m.floor(random.random()*size))
+            end_pc_py = time.perf_counter()
+
+
+            moy_me += end_pc-start_pc
+            moy_py += end_pc_py-start_pc_py
+        moy_me = moy_me / nb
+        moy_py = moy_py / nb
+        ans[0].append(moy_me)
+        ans[1].append(moy_py)
+    return ans
+
+
+
+
+
+
+
+
+
+
+
+def testing_unit_mix():
+    lists_size =  [10, 500, 5000, 50000, 100000]
+    timeming = perf_mix(partie1.mix_list, random.shuffle, lists_size, 10)
+    print(timeming[0])
+    print(timeming[1])
+
+    fig,ax = plt.subplots()
+
+    ax.plot(lists_size,timeming[0],'bo-',label='partie1.mix_list')
+    ax.plot(lists_size,timeming[1], 'r*-', label='random.shuffle')
+    ax.set(xlabel='nombre de test', ylabel='temps d\'execution',
+    title='le temps d\'execution pour different fonction')
+    ax.legend(loc='upper center', shadow=True, fontsize='x-large')
+    #fig.savefig("test.png")
+    plt.show()
+
+def testing_unit_extract():
+    lists_size =  [10, 500, 5000, 50000, 100000]
+    timeming = perf_extract(partie1.extract_elements_list, random.sample, lists_size, 10)
+    print(timeming[0])
+    print(timeming[1])
+
+    fig,ax = plt.subplots()
+
+    ax.plot(lists_size,timeming[0],'bo-',label='partie1.extract_elements_list')
+    ax.plot(lists_size,timeming[1], 'r*-', label='random.sample')
+    ax.set(xlabel='nombre de test', ylabel='temps d\'execution',
+    title='le temps d\'execution pour different fonction')
+    ax.legend(loc='upper center', shadow=True, fontsize='x-large')
+    #fig.savefig("test.png")
+    plt.show()
+
+
+def main():
+    testing_unit_extract()
+
+
+if __name__ == "__main__":
+    main()
